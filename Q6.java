9c6848604bd1ac7b33991d29397e7f5a32e4e72b
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-06-05 21:55:01 +0600

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -1,7 +1,6 @@
 
 //Write a Java program to create a class called "Employee" with a name, job title, and salary attributes, and methods to calculate and update salary.
 
-
 //Employee.java
 // Define the Employee class
 public class Employee {
