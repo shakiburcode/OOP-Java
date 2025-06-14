45c9cd840fb75f90de5feae6d388528415b41e48
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-06-14 23:57:40 +0600

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -9,6 +9,8 @@ public class Account {
     // Declare a private variable to store the balance of the account
     private double balance;
 
+    
+
     // Constructor for the Account class that initializes the name, account number, and balance variables
     public Account(String name, String accountNumber, double balance) {
         // Set the name variable to the provided name parameter
